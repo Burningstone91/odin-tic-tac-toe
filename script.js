eb23b7f6cc2979ced7c6a4353857fe24ed2f7fd7
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-04-14 01:17:31 +0200

    Add class for creating the Gameboard incl. method to place a Player's sign on the board

--- script.js
+++ script.js
@@ -4,11 +4,33 @@ class Player {
     this.id = id;
   }
 
-  setToken(token) {
-    this.token = token;
+  setSign(sign) {
+    this.sign = sign;
   }
 }
 
-const Player1 = new Player("User", 1);
-Player1.setToken("x");
-console.log(Player1);
+/* Gameboard is an array of arrays, each array represents a row */
+class Gameboard {
+  constructor() {
+    this.board = Array(9).fill(0);
+  }
+
+  getBoardStatus() {
+    return this.board;
+  }
+
+  placeSignOnBoard(playerId, position) {
+    if (this.board[position] == 0) {
+      this.board[position] = playerId;
+    }
+  }
+}
+
+const player1 = new Player("User", 1);
+player1.setSign("x");
+console.log(player1);
+
+const game = new Gameboard();
+game.placeSignOnBoard(1, 6);
+game.placeSignOnBoard(2, 8);
+console.log(game.getBoardStatus());
